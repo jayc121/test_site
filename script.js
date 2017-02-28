c8568c6c9959bf72742c4137526da0de8434775e
jayc121 <jaiyeesh@gmail.com>
2017-02-28 14:13:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,97 @@
+// Default values
+function orderChickenWith(sideDish) {
+  sideDish = sideDish || "whatever!";
+  console.log("Chicken with " + sideDish);
+}
+
+orderChickenWith("noodles");
+orderChickenWith();
+// Object creation
+var company = new Object();
+company.name = "Facebook";
+company.ceo = new Object();
+company.ceo.firstName = "Mark";
+company.ceo.favColor = "blue";
+
+console.log(company);
+console.log("Company CEO name is: " 
+  + company.ceo.firstName);
+
+console.log(company["name"]);
+var stockPropName = "stock of company";
+company[stockPropName] = 110;
+
+console.log("Stock price is: " + 
+  company[stockPropName]);
+
+// Better way: object literal
+var facebook = {
+  name: "Facebook",
+  ceo: {
+    firstName: "Mark",
+    favColor: "blue"
+  },
+  "stock of company": 110
+};
+
+console.log(facebook.ceo.firstName);
+// Copy by Reference vs by Value
+var a = 7;
+var b = a;
+console.log("a: " + a);
+console.log("b: " + b);
+
+b = 5;
+console.log("after b update:");
+console.log("a: " + a);
+console.log("b: " + b);
+
+
+
+var a = { x: 7 };
+var b = a;
+console.log(a);
+console.log(b);
+
+b.x = 5;
+console.log("after b.x update:");
+console.log(a);
+console.log(b);
+
+
+
+
+
+
+// Pass by reference vs by value
+function changePrimitive(primValue) {
+  console.log("in changePrimitive...");
+  console.log("before:");
+  console.log(primValue);
+  
+  primValue = 5;
+  console.log("after:");
+  console.log(primValue);
+}
+
+var value = 7;
+changePrimitive(value); // primValue = value
+console.log("after changePrimitive, orig value:");
+console.log(value);
+
+
+
+function changeObject(objValue) {
+  console.log("in changeObject...");
+  console.log("before:");
+  console.log(objValue);
+  
+  objValue.x = 5;
+  console.log("after:");
+  console.log(objValue);
+}
+
+value = { x: 7 };
+changeObject(value); // objValue = value
+console.log("after changeObject, orig value:");
+console.log(value);
