d09c049c4ed040f4b015762df9e95aea08c54dc9
jayc121 <jaiyeesh@gmail.com>
2017-03-21 15:18:25 +0530

    Update m2.js

--- js/m2.js
+++ js/m2.js
@@ -9,12 +9,26 @@ document.addEventListener("DOMContentLoaded",
         // Call server to get the name
         $ajaxUtils
           .sendGetRequest("data/name.json", 
-            function (res) {
-          if(res.student1){
-              var message = 
-                res.student1
-             var message2 = res.marks
-             } 
+           var name =
+   document.getElementById("name").value;
+   var message = "<h2>" + name + "!</h2>";
+
+  
+
+  document
+    .getElementById("content")
+    .innerHTML = message;
+
+  if (name === res.student1) {
+    var title = 
+      document
+        .querySelector("#title")
+        .textContent;
+    title +=;
+    document
+        .querySelector("h1")
+        .textContent = marks;
+  }
 
               document.querySelector("#content")
                 .innerHTML = "<h2>" + message + "</h2>";
